142fe020de4fc2ed4fc7a393a2155932965451cc
Kamil Kaczmarek <kkaczmarek631@gmail.com>
2023-04-15 10:52:59 +0200

    fixed video reading

--- aruco_finder.py
+++ aruco_finder.py
@@ -3,33 +3,34 @@ import numpy as np
 import cv2 as cv
 import matplotlib
 
-
 class ArucoFinder:
 
     def __init__(self):
         self.aruco_data = csv.reader(open('data/aruco_positions.csv'))
         self.video_frame = cv.VideoCapture("video/vid.MP4")
+        self.totalframecount= int(self.video_frame.get(cv.CAP_PROP_FRAME_COUNT))
 
-
-
+i = 0
 
 if __name__ == '__main__':
     finder = ArucoFinder()
+
 while True:
     
     if finder.video_frame.isOpened():
         ret, frame = finder.video_frame.read()
-    if not ret:
+
+    if i == finder.totalframecount:
         print("Can't receive frame (stream end?). Exiting ...")
         break
-    frame = cv.resize(frame,[640,480])
-    cv.imshow('frame', frame)
+
+    if frame is not None:
+        frame = cv.resize(frame,[640,480])
+        cv.imshow('frame', frame)
+        i = i + 1
+
     if cv.waitKey(25) & 0xFF == ord('q'):
         break
 
 finder.video_frame.release()
 cv.destroyAllWindows()
-
-    
-
-
