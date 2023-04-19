bee4a2f2fb2e88953acd4fb8c50bc7abc2c36b39
studentairu13 <dajurowski@gmail.com>
2023-04-19 21:41:26 +0200

    added detecting aruco markers

--- aruco_finder.py
+++ aruco_finder.py
@@ -7,8 +7,11 @@ class ArucoFinder:
 
     def __init__(self):
         self.aruco_data = csv.reader(open('data/aruco_positions.csv'))
-        self.video_frame = cv.VideoCapture("video/vid.MP4")
+        self.video_frame = cv.VideoCapture("/home/dawid/Pobrane/GX010280.MP4")
         self.totalframecount= int(self.video_frame.get(cv.CAP_PROP_FRAME_COUNT))
+        self.aruco_dict = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_1000)
+        self.aruco_params = cv.aruco.DetectorParameters()
+        self.detector = cv.aruco.ArucoDetector(self.aruco_dict, self.aruco_params)
 
 i = 0
 
@@ -26,6 +29,18 @@ while True:
 
     if frame is not None:
         frame = cv.resize(frame,[640,480])
+        (corners, ids, rejected) = finder.detector.detectMarkers(frame)
+        if len(corners) > 0:
+            ids = ids.flatten()
+            for (one_corner, one_id) in zip(corners, ids):
+                one_corner = one_corner.reshape(4, 2)
+                one_corner=one_corner.astype(int)
+                (top_left, top_right, bottom_left, bottom_right) = one_corner
+                cv.line(frame, top_left, top_right, (0, 255, 0), 2)
+                cv.line(frame, top_right, bottom_right, (0, 255, 0), 2)
+                cv.line(frame, bottom_right, bottom_left, (0, 255, 0), 2)
+                cv.line(frame, bottom_left, top_left, (0, 255, 0), 2)
+
         cv.imshow('frame', frame)
         i = i + 1
 
