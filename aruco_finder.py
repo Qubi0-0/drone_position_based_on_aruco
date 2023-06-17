78b31c337b644cd3794d1e13d18f822a1714fc62
Qubi <kuba1102@gmail.com>
2023-06-17 12:02:42 +0200

    new branch created

--- aruco_finder.py
+++ aruco_finder.py
@@ -65,55 +65,64 @@ class ArucoFinder:
 
     def __init__(self):
         self.aruco_data = pd.read_csv('data/aruco_positions.csv').set_index('Marker_ID')
+        self.mocap_data = pd.read_csv('data/mocap_ref_data.csv')
         self.video_frame = cv.VideoCapture("video/vid.MP4")
         self.total_frame_count= int(self.video_frame.get(cv.CAP_PROP_FRAME_COUNT))
         self.aruco_dict = cv.aruco.getPredefinedDictionary(cv.aruco.DICT_4X4_1000)
         self.aruco_params = cv.aruco.DetectorParameters()
         self.detector = cv.aruco.ArucoDetector(self.aruco_dict, self.aruco_params)
+        
+        self.camera_fixed_positions = []
+
+    def check_camera_pose(self):
+        pass
+
 
 if __name__ == '__main__':
     finder = ArucoFinder()
-i = 0
-cam_positions = []
+    i = 0
+    cam_positions = []
 
-while True:
-    
-    if finder.video_frame.isOpened():
-        ret, frame = finder.video_frame.read()
+    while True:
+        
+        if finder.video_frame.isOpened():
+            ret, frame = finder.video_frame.read()
 
-    if i == finder.total_frame_count:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
+        if i == finder.total_frame_count:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
 
-    if frame is not None:
-        (corners, ids, rejected) = finder.detector.detectMarkers(frame)
-        if len(corners) > 0:
-            ids = ids.flatten()
-            points_2D = []
-            points_3D = []
-            for (one_corner, one_id) in zip(corners, ids):
-                if (one_id) in range(1, 83):
-                    (top_left, top_right, bottom_right, bottom_left) = getCorners(one_corner)
-                    drawLines(frame, top_left, top_right, bottom_right, bottom_left)
-                    drawIds(frame, one_id, top_left, top_right, bottom_right, bottom_left)
-                    points_2D.append((top_right)/1.0)
-                    points_3D.append(get3DPoints(one_id, finder.aruco_data))
-                    
-            if len(points_2D) >= 4 and len(points_3D) == len(points_2D):
-                _, rvec, tvec = cv.solvePnP(np.array(points_3D), np.array(points_2D), camera_matrix, None)
-                R, _ = cv.Rodrigues(rvec)
-                camera_pos = np.dot(np.array([(1, 0, 0),
-                          (0, -1, 0),
-                          (0, 0, -1)]), np.dot(np.array(-R), tvec))
-                cam_positions.append(camera_pos)
+        if frame is not None:
+            (corners, ids, rejected) = finder.detector.detectMarkers(frame)
+            if len(corners) > 0:
+                ids = ids.flatten()
+                points_2D = []
+                points_3D = []
+                for (one_corner, one_id) in zip(corners, ids):
+                    if (one_id) in range(1, 83):
+                        (top_left, top_right, bottom_right, bottom_left) = getCorners(one_corner)
+                        drawLines(frame, top_left, top_right, bottom_right, bottom_left)
+                        drawIds(frame, one_id, top_left, top_right, bottom_right, bottom_left)
+                        points_2D.append((top_right)/1.0)
+                        points_3D.append(get3DPoints(one_id, finder.aruco_data))
+                        
+                if len(points_2D) >= 4 and len(points_3D) == len(points_2D):
+                    _, rvec, tvec = cv.solvePnP(np.array(points_3D), np.array(points_2D), camera_matrix, None)
+                    R, _ = cv.Rodrigues(rvec)
+                    camera_pos = np.dot(np.array(
+                            [(1, 0, 0),
+                            (0, -1, 0),
+                            (0, 0, -1)]), np.dot(np.array(-R), tvec))
+                    cam_positions.append(camera_pos)
 
-        frame = cv.resize(frame,[640,480])
-        cv.imshow('frame', frame)
-        i += 1
+            frame = cv.resize(frame,[640,480])
+            cv.imshow('frame', frame)
+            print(finder.mocap_data)
+            i += 1
 
-    if cv.waitKey(25) & 0xFF == ord('q'):
-        break
+        if cv.waitKey(25) & 0xFF == ord('q'):
+            break
 
-finder.video_frame.release()
-cv.destroyAllWindows()  
-plot_trajectory(cam_positions)
+    finder.video_frame.release()
+    cv.destroyAllWindows()  
+    plot_trajectory(cam_positions)
